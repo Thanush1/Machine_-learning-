f48aa41cc8ccb4eadec556e8119922c46889045b
Thanush1 <60345529+Thanush1@users.noreply.github.com>
2020-01-29 23:11:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-from model import *
+import tensorflow as tf 
+import numpy as np 
+
+def fizz_buzz_encode(num):  
+    if num % 3 == 0 and num %5 == 0:
+        return np.array([1,0,0,0])
+    if num % 3 == 0:
+        return np.array([0,1,0,0])
+    if num % 5 == 0:
+        return np.array([0,0,1,0])
+    return np.array([0,0,0,1])
+
+def binary_encode(num,num_digits):
+    activations = np.array([num>>d&1 for d in range(num_digits)])
+    return activations 
+
+
 def logic_fizzbuzz(N):
     file = open('Software1.txt','w')
     for i in range(1,N+1):
@@ -14,9 +30,18 @@ def logic_fizzbuzz(N):
     file.close()
     
 
+# load json and create model
+json_file = open('weight.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+model1 = tf.keras.models.model_from_json(loaded_model_json,custom_objects = None )
+# load weights into new model
+model1.load_weights("model.h5")
 logic_fizzbuzz(100)
-import sys 
 
+
+import sys 
+#str(sys.argv[1]
 test_input = open(str(sys.argv[1]),'r').read().strip()
 string = test_input.split("\n")
 values = [int(x) for x in string]
@@ -26,7 +51,7 @@ test_y = np.array([fizz_buzz_encode(x) for x in values])
 
 answer = model1.predict(test_x)
 
-validation = model1.evaluate(test_x,test_y)
+#validation = model1.evaluate(test_x,test_y)
 
 
 def fizz_buzz_ML(N):
@@ -47,6 +72,6 @@ def fizz_buzz_ML(N):
 fizz_buzz_ML(101)
 
 import os
-repo_path = os.path.dirname(os.path.abspath(__file__))
+repo_path = os.path.dirname(os.path.abspath("main.py"))
 out_sw_1_fp = repo_path + "/Software1.txt"
 out_sw_2_fp = repo_path + "/Software2.txt"
