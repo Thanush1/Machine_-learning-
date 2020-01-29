54960d4e07293659a134d0e7cfbb42d1c7a9a95a
Thanush1 <60345529+Thanush1@users.noreply.github.com>
2020-01-29 23:41:35 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,11 @@
 import tensorflow as tf 
 import numpy as np 
-import sys 
+from tensorflow import keras 
+num_digits = 10
+def binary_encode(num,num_digits):
+    activations = np.array([num>>d&1 for d in range(num_digits)])
+    return activations 
+    
 
 def fizz_buzz_encode(num):  
     if num % 3 == 0 and num %5 == 0:
@@ -11,70 +16,28 @@ def fizz_buzz_encode(num):
         return np.array([0,0,1,0])
     return np.array([0,0,0,1])
 
-def binary_encode(num,num_digits):
-    activations = np.array([num>>d&1 for d in range(num_digits)])
-    return activations 
+training_X = np.array([binary_encode(x,num_digits) for x in range(101,1001)])
+training_Y = np.array([fizz_buzz_encode(i) for i in range(101,1001)])
+
+def init_weights(shape):
+    return tf.Variable(tf.random.normal(shape,stddev = 0.01 ))
 
 
-test_input = open(str(sys.argv[1]),'r').read().strip()
-string = test_input.split("\n")
-values = [int(x) for x in string]
+model2= tf.keras.models.Sequential([keras.layers.Dense(units =250 ,input_shape =([10]),activation  = 'relu'),
+                                    keras.layers.Dense(units = 120,activation  = 'relu'),
+                                    #keras.layers.Dense(units = 50 ,input_shape = ([10]),activation  = 'relu'),
+                                    #keras.layers.Dense(units = 20 ,input_shape = ([10]),activation  = 'relu'),
+                            keras.layers.Dense(units = 4,activation = 'softmax')
+                            ])
+model2.compile(loss = 'categorical_crossentropy',optimizer='adam', metrics=['accuracy'])
+model2.fit(training_X,training_Y,epochs = 100)
 
-def logic_fizzbuzz(N):
-    file = open('Software1.txt','w')
-    for i in values :
-        if i % 3 == 0 and i % 5 == 0:
-            file.write("fizzbuzz" + '\n')
-        elif i % 3 == 0:
-            file.write("fizz" + '\n')
-        elif i % 5 == 0:
-            file.write("buzz" + '\n')
-        else:
-            file.write(str(i) + '\n')
-            
-    file.close()
-    
-
-# load json and create model
-json_file = open('weight.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-model1 = tf.keras.models.model_from_json(loaded_model_json,custom_objects = None )
-# load weights into new model
-model1.load_weights("model.h5")
-logic_fizzbuzz(100)
+#model2.predict(binary_encode(5,10).reshape(1,10))
 
 
-
-#str(sys.argv[1]
-
-test_x = np.array([binary_encode(x,10) for x in values])
-test_y = np.array([fizz_buzz_encode(x) for x in values])
-
-answer = model1.predict(test_x)
-
-#validation = model1.evaluate(test_x,test_y)
-
-
-def fizz_buzz_ML(N):
-    file = open("Software2.txt",'w')
-    for i in range(1,N):
-        index = np.unravel_index(answer[i-1].argmax(),answer[i-1].shape)
-        
-        if index == (0,):
-            file.write("fizzbuzz" + '\n')
-        elif index == (1,):
-            file.write("fizz" + '\n')
-        elif index == (2,):
-            file.write("buzz" + '\n')
-        else:
-            file.write(str(values[i-1]) + '\n')
-    file.close()
-            
-fizz_buzz_ML(101)
-
-import os
-repo_path = os.path.dirname(os.path.abspath("main.py"))
-out_sw_1_fp = repo_path + "/Software1.txt"
-out_sw_2_fp = repo_path + "/Software2.txt"
+model_json = model2.to_json()
+with open("weight.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model2.save_weights("model.h5")
 
